a007ac1cd6059a8c740f56b46f6687ecc88b6146
P Hemanth Kumar <80844476+hemanth4134@users.noreply.github.com>
2025-04-26 14:28:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -173,15 +173,15 @@ resource "aws_lb" "app_lb" {
 # Target Group
 
 
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200"
-  }
-}
+ # health_check {
+  #  path                = "/"
+   # interval            = 30
+ #   timeout             = 5
+  #  healthy_threshold   = 2
+  #  unhealthy_threshold = 2
+  #  matcher             = "200"
+  #}
+#}
 
 # Listener
 resource "aws_lb_listener" "app_listener" {
