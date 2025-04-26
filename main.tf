9b5fb43cda167ded5d4a627fd5abd4676d659d63
P Hemanth Kumar <80844476+hemanth4134@users.noreply.github.com>
2025-04-26 14:12:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -86,20 +86,20 @@ resource "aws_ecs_cluster" "main" {
 }
 
 # IAM Role for Fargate task
-resource "aws_iam_role" "ecs_task_exec_role" {
-  name = "ecsTaskExecutionRole"
+#resource "aws_iam_role" "ecs_task_exec_role" {
+ # name = "ecsTaskExecutionRole"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
+  #assume_role_policy = jsonencode({
+   # Version = "2012-10-17",
+    #Statement = [{
+     # Effect = "Allow",
+      #Principal = {
+       # Service = "ecs-tasks.amazonaws.com"
+      #},
+      #Action = "sts:AssumeRole"
+    #}]
+  #})
+#}
 
 resource "aws_iam_role_policy_attachment" "ecs_exec_attach" {
   role       = aws_iam_role.ecs_task_exec_role.name
